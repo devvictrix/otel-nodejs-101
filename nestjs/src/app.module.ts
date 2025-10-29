770eb3231b190e29020ee13dd50ec0bfa1d34ff6
Panupong Jaengaksorn <dev.victrix@gmail.com>
2025-10-29 19:28:48 +0700

    save

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersService } from './user.service';
+import { PostsService } from './post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService, AppService, UsersService, PostsService],
 })
 export class AppModule {}
